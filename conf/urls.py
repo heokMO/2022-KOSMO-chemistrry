053178fc7ea8787ab1375af681b5bbf953c7f827
chemistrry <gurah1220@gmail.com>
2022-01-20 17:52:09 +0900

    thrid commit

--- conf/urls.py
+++ conf/urls.py
@@ -20,5 +20,5 @@ import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('mypage.urls')),
 ]
---
+++ mypage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from mypage import views
+
+urlpatterns = [
+    path("",views.login),
+    path("mainpage",views.mainpage),
+    path("memberjoin",views.memberjoin),
+]
