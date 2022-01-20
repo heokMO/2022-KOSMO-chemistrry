581a46b9c57862db25b93e6d3cb88f53c1cf6d63
chemistrry <gurah1220@gmail.com>
2022-01-20 15:35:27 +0900

    second commit

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l_2912qhj$$c(rd!6+xytb&c-$#tuoaxo@mrjf!pb$+y#gr3xn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
